50faebc9b61a56175e127aff5d6d53793261d8d2
konstantinAtMaestroTech <konstantin@maestro-tech.com>
2024-09-13 13:01:38 +0200

    deployment

--- app/lib/db/pool.ts
+++ app/lib/db/pool.ts
@@ -1,46 +1,32 @@
-import mysql from 'mysql2/promise'
+import mysql from 'mysql2/promise';
 
-export const pool = mysql.createPool({
-    host: process.env.DEV_MYSQL_HOST,
-    user: process.env.DEV_MYSQL_USERNAME,
-    database: process.env.DEV_MYSQL_DATABASE,
-    password: process.env.DEV_MYSQL_PASSWORD,
+const isProduction = process.env.NODE_ENV === 'production';
+
+const config = {
+    host: isProduction ? process.env.PROD_MYSQL_HOST : process.env.DEV_MYSQL_HOST,
+    user: isProduction ? process.env.PROD_MYSQL_USERNAME : process.env.DEV_MYSQL_USERNAME,
+    password: isProduction ? process.env.PROD_MYSQL_PASSWORD : process.env.DEV_MYSQL_PASSWORD,
     waitForConnections: true,
     connectionLimit: 10,
-    maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-    idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+    maxIdle: 10,
+    idleTimeout: 60000,
     queueLimit: 0,
     enableKeepAlive: true,
     keepAliveInitialDelay: 0,
     multipleStatements: true,
+};
+
+export const pool = mysql.createPool({
+    ...config,
+    database: isProduction ? process.env.PROD_MYSQL_DATABASE : process.env.DEV_MYSQL_DATABASE,
 });
 
 export const pool_chat = mysql.createPool({
-    host: process.env.DEV_MYSQL_HOST,
-    user: process.env.DEV_MYSQL_USERNAME,
-    database: process.env.DEV_MYSQL_DATABASE_CHAT,
-    password: process.env.DEV_MYSQL_PASSWORD,
-    waitForConnections: true,
-    connectionLimit: 10,
-    maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-    idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
-    queueLimit: 0,
-    enableKeepAlive: true,
-    keepAliveInitialDelay: 0,
-    multipleStatements: true,
+    ...config,
+    database: isProduction ? process.env.PROD_MYSQL_DATABASE_CHAT : process.env.DEV_MYSQL_DATABASE_CHAT,
 });
 
 export const pool_auth = mysql.createPool({
-    host: process.env.DEV_MYSQL_HOST,
-    user: process.env.DEV_MYSQL_USERNAME,
-    database: process.env.DEV_MYSQL_DATABASE_AUTH,
-    password: process.env.DEV_MYSQL_PASSWORD,
-    waitForConnections: true,
-    connectionLimit: 10,
-    maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-    idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
-    queueLimit: 0,
-    enableKeepAlive: true,
-    keepAliveInitialDelay: 0,
-    multipleStatements: true,
+    ...config,
+    database: isProduction ? process.env.PROD_MYSQL_DATABASE_AUTH : process.env.DEV_MYSQL_DATABASE_AUTH,
 });
